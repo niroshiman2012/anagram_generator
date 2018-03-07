f33bdf9da62103a3e5364ace9ae6bcd4768680cb
niroshiman2012 <niroshiman2012@gmail.com>
2018-03-07 23:49:24 +1100

    Added some comments, renamed some variables, added description and instruction.

--- word_game.py
+++ word_game.py
@@ -1,39 +1,47 @@
 # Author : Niroshiman
+# Descri : Code that generates anagram from any word. 
+# Instru : 1) Set type_word to personally desired word  
+#  		   2) Set valid_word_length to desired number
 
-filename = 'words.txt'
-
-# stored all the words from the text file into list
-with open(filename) as file_object:
-
-	words = file_object.readlines()
-	
-# a function that takes every alphabet from a username and puts into a list
+# A function that takes every alphabet from a username and puts into a list
 def make_words(name):
 
-	name = name.upper()	# conver name to an uppercase string
-	list = []	# this is an empty list
+	name = name.upper()	# convert name to an uppercase string
+	name_alpha_list = []	# this is an empty list
 	
-	for alpha in name:	# take the current alphabet in name...
-		list.append(alpha)	# and put it in the list
+	for alpha in name:	# take the current alphabet in name
+		name_alpha_list.append(alpha)	# and put it in the list
 
-	return list	# once the list is complete return the list
-
-name = make_words('insertword')	# returns a list called name
-total = 0
+	return name_alpha_list	# once the list has all alphabets from name, return it
 
 
-# take the current word in the list of words
+# Set the file name as the name of the English dictionary in use.
+filename = 'words.txt'
+
+# Set desired word which will be used to generate anagram
+type_word =  'insertword'
+name = make_words(type_word)	# returns a list called name
+total = 0	# number of anagrams generated 
+valid_word_length = 0 # set desired word length of a generated anagram
+
+# Stored all the words from the text file into a list.
+with open(filename) as file_object:
+	words = file_object.readlines()
+	
+
+# Take the current word in the list of words
 for word in words:
 
+	name = make_words(type_word)
 	
-	# eliminating the next line character from end of the word, word is a string FYI
+	# Eliminating the next line character from end of the word, word is a string 
 	word = word.replace('\n',"").upper() # convert word to an uppercase string
 	word = make_words(word) # converting the word from string to list
 
-	# set current count to length of word
+	# Set count to length of current word
 	count = len(word)
 
-	# for the current alphabet in the word from the dictionary...
+	# For the current alphabet in the word from the dictionary...
 	for alpha in word:
 
 		# if this current alphabet in word is found in the name list...
@@ -44,37 +52,13 @@ for word in words:
 	# check if the count has reached zero...
 	# indicating all alphabets in word exists in name
 	if count == 0:
-		# print(word)	# print that word to double confirm
+
 		total += 1
 
-		if len(word) > 0:
+		if len(word) > valid_word_length:
 			word = ''.join(word) # convert list to string
-			print(word)
+			print(word)	# print the word
 
-	name = make_words('insertword')
 
-name = ''.join(name)
+name = ''.join(name) # convert list to string
 print('total number of words found in ' + name + ': ' + str(total))
-
-
-
-# draft
-
-# word = make_words('boshi')	# returns a list called word
-# print(name)
-# print(word)
-# match_count = len(word)
-
-# # for the current alphabet in the word from the dictionary...
-# for alpha in word:
-
-# 	# if this current alphabet in word is found in the name list...
-# 	if alpha in name:
-# 		print('yes')	# print yes to indicate it exists in the name list...
-# 		name.remove(alpha)	# and remove the current alphabet from the name list...
-# 		print(name)	# this is the new name list with one of the alphabets removed...
-# 	else:
-# 		print('no') # print no to indicate it does not exist
-# 		print(name)		# nothing to remove from the name list, print it anyways
-
-# print(name)
